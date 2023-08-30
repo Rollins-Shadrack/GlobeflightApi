56419ec0005b3d1e345bad3b79cc621ea1f1bf13
Rollins-Shadrack <rshadrackochieng@gmail.com>
2023-08-30 09:38:36 +0300

    5 commit

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken')
 
 const generateToken = (res, userId) =>{
-    const token = jwt.sign({userId}, process.env.JWT_SECRET,{
+    const token = jwt.sign({userId}, 'sjdioadkcdidocmiqwejddanoiciqwencjaokejdncjaomwoedjincxjasdncnsd',{
         expiresIn: '2d'
     })
 
